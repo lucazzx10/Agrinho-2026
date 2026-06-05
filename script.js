b7d1c9b74418478a5867932ed2311445d6903811
lucazzx10 <lucas.gomes.silva20@escola.pr.gov.br>
2026-06-05 15:24:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function mostrarMensagem() {
+
+    alert(
+        "A agroecologia promove equilíbrio entre produção agrícola, preservação ambiental e desenvolvimento social."
+    );
+
+}
+
+function contarBeneficios() {
+
+    let beneficios = 5;
+
+    document.getElementById("resultado").innerHTML =
+    "O site apresenta " + beneficios + " benefícios da agroecologia.";
+
+}
